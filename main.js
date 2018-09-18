3a48615dbd65852373e12a9e9a8ae3ada09e2c97
ThaGary <garyanderson31591@gmail.com>
2018-09-18 15:38:18 -0600

    added clear button

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
+// variables
 var boxes = document.querySelectorAll('td')
 var selection = document.querySelectorAll('.dot')
-
-// selection
+var btn = document.querySelector('button')
+// color selector
 for (var i = 0; i < selection.length; i++) {
     selection[i].addEventListener("click", selector)
 }
-
+// color selector function
 function selector(event) {
     for (var i = 0; i < selection.length; i++) {
         selection[i].style.border = '3px solid lightgray'
     }
     event.target.style.border = '3px solid black'
     color = event.target.id
-
 }
+// box selection
 for (var i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener("click", colorchange)
 }
-
-function colorchange() {
+// box color changer
+function colorchange(event) {
     event.target.style.backgroundColor = color
+}
+// clear eventlistener
+btn.addEventListener("click", clear)
+// clear function
+function clear() {
+    window.location.reload()
 }
